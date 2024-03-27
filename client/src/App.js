cf7501537242bc5ac10b4147e04bfcb326cea7a5
Som <somjeed0546@gmail.com>
2024-03-28 05:46:29 +0700

    เพิ่ม NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './Navbar';
-import Home from './Home';
-import CostumeList from './CostumeList';
-import CostumeTypeList from './CostumeTypeList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComp from './Navbar';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/costumes" element={<CostumeList />} />
-          <Route path="/costume-types" element={<CostumeTypeList />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <NavBarComp/>
+    </div>
   );
 };
 
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,23 +1,42 @@
-// NavBar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import {Navbar,Nav} from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import CostumeForm from './CostumeForm';
+import CostumeList from './CostumeList';
+import CostumeTypeList from './CostumeTypeList';
 
-const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/costumes">Costumes</Link>
-        </li>
-        <li>
-          <Link to="/costume-types">Costume Types</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+export default class NavbarComp extends Component{
+    render(){
+        return(
+            <Router>
+            <div>
+                <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/costumeForm"}>Costume Form</Nav.Link>
+            <Nav.Link as={Link} to={"/costumeList"}>Costume List</Nav.Link>
+            <Nav.Link as={Link} to={"/costumeTypeList"}>Costume Type List</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 
-export default NavBar;
+            </div>
+            <div>
+                <Routes>
+                <Route path="/costumeForm" element={<CostumeForm/>}>
+            </Route>
+            <Route path="/costumeList" element={<CostumeList/>}>
+            </Route>
+            <Route path="/costumeTypeList" element={<CostumeTypeList/>}>
+            </Route>
+            <Route path="/" element={<Home/>}>
+            </Route>
+            </Routes>
+            </div>
+            </Router>
+        )
+    }
+}
