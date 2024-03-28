8eb61c75a49b84f0836b8e16d75f78930323f8dd
mon2384 <152243939+bart2559b@users.noreply.github.com>
2024-03-28 18:13:56 +0700

    git

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 // @EnableSwagger2
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
